ef4a4a83cd75cc843a54bcaa984800cbbe3e56a9
e23e <merbin.russel@freshworks.com>
2023-09-11 15:47:21 +0530

    Empty commit

--- libs/utils.py
+++ libs/utils.py
@@ -5,6 +5,7 @@ LOG_FORMAT = "%(asctime)s - %(levelname)-8s [%(filename)s:%(lineno)d] - %(messag
 def get_logger():
     logging.basicConfig(filename="log.log",level="INFO",format=LOG_FORMAT)
     logger = logging.getLogger("Sub Monitor")
+    
     return logger
 
 
